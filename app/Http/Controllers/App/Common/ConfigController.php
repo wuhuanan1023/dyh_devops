076f0de8ca51b856cda6450e1c27468b29c23006
wuhuanan1023 <wuhuanan1023@163.com>
2023-07-20 16:50:04 +0800

    添加类

---
+++ app/Http/Controllers/App/Common/ConfigController.php
@@ -0,0 +1,19 @@
+<?php
+
+namespace App\Http\Controllers\App\Common;
+
+use App\Http\Controllers\App\BaseController;
+
+class ConfigController extends BaseController
+{
+
+    /**
+     * 初始化
+     * @return mixed
+     */
+    public function configInit()
+    {
+
+    }
+
+}
